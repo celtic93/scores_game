9cf4b42aec81308acb4d50cfc0200d2be0a661b5
celtic93 <egorka80@list.ru>
2023-11-04 14:37:10 +0300

    Fixed LiveResults

--- app/sidekiq/live_results.rb
+++ app/sidekiq/live_results.rb
@@ -4,7 +4,7 @@ class LiveResults
   def perform
     Round.active.each do |round|
       round.matches.ordered_by_id.each do |match|
-        result = Match::Monitor.new(round, round.matches.last).check_score
+        result = Match::Monitor.new(round, match).check_score
         Telegram.bot.send_message(chat_id: round.chat_id, text: result.message) if result.message.present?
       end
     end
